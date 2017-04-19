6a77cb811f5635d35467845efe5d367fa067f36b
daniel_6287@hotmail.com <administradorcito@localhost.localdomain>
2017-04-19 11:51:03 -0500

    Otro bash

---
+++ app4.js
@@ -0,0 +1,14 @@
+{
+    "id": "write-2"
+    "cmd":"time bash otrowrite.bash",
+    "cpus": 5,
+    "mem": 10.0,
+    "instances": 2,
+    "uris": [
+
+        "https://raw.githubusercontent.com/Dauzy/Mesos_script/master/otrowrite.bash"
+
+    ]
+
+}
+
---
+++ otrowrite.bash
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+inicio=`date +%s`
+
+a=1
+
+while [ $a -le 1000000 ]
+
+do
+
+        echo $a
+
+        echo "Escribiendo... $a">>archivo.txt
+
+        (( a++ ))
+
+done
+
+fin=`date +%s`
+
+let total=$fin-$inicio
+
+      
+
+echo "ha tardado: $total segundos"
